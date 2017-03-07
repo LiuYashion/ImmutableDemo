ce11dd98535929c28d6effd910099fa5bd95eca3
LiuYashion <2472906920@qq.com>
2017-03-07 19:58:06 +0800

    add
    tbc

--- fromJS.js
+++ fromJS.js
@@ -1,6 +1,19 @@
 
 var Immutable = require('immutable');
-var map1 = Immutable.Map({a:1, b:2, c:3});
-var map2 = map1.set('b', 50);
-map1.get('b'); // 2
-map2.get('b'); // 50
+
+/**
+ * { fromJS(json: any, reviver?: (k: any, v: Iterable<any, any>) => any): any }
+ * 
+ * {If reviver is not provided, the default behavior will convert Arrays into Lists and Objects into Maps.}
+ */
+
+//	键值有索引,可迭代.那么转成list.反之装成map
+var test1 =	Immutable.fromJS({a: {b: [10, 20, 30]}, c: 40}, function (key, value) {
+  var isIndexed = Immutable.Iterable.isIndexed(value);
+  return isIndexed ? value.toList() : value.toOrderedMap();
+});
+//OrderedMap { "a": OrderedMap { "b": List [ 10, 20, 30 ] }, "c": 40 }
+
+var test2 =	Immutable.fromJS({a: {b: [10, 20, 30]}, c: 40});
+
+console.log( test2 )
