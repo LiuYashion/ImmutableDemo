482b8b5ffcb88a188f9e10c6fa9929ca10c5e0f1
LiuYashion <2472906920@qq.com>
2017-03-10 11:47:25 +0800

    add
    tbc

--- hash.js
+++ hash.js
@@ -1,9 +1,28 @@
 
 
-var Immutable = require('immutable');
+//var Immutable = require('immutable');
+//
+//var c = Immutable.hash(13)
+//console.log(c)
 
-var c = Immutable.hash(13)
-console.log(c)
+
+
+
+import fetch from 'isomorphic-fetch'
+
+fetch(url, {
+	method: "POST",
+  	mode: "no-cors",
+  	headers: {
+    	"Content-Type": "application/x-www-form-urlencoded"
+  	},
+  	body: "请求参数"
+}).then(function(res) {
+ 	console.log("Response succeeded?", JSON.stringify(res.ok));
+ 	console.log(JSON.stringify(res));
+}).catch(function(e) {
+ 	console.log("fetch fail", JSON.stringify(params));
+});
 
 
 
